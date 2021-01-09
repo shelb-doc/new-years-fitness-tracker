718905352bba3aeeb370adf8e6d32fc7a1a6f825
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2021-01-09 11:32:44 -0500

    refactored index.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,13 +1,21 @@
 init();
 
 async function init() {
+  // location.search = return the querystring part of a URL, e.g., after ?
+  console.log('location.search.split("=")[1]: ', location.search.split("=")[1]);
+  
+  // If there is no location.search, then getLastWorkout()
   if (location.search.split("=")[1] === undefined) {
+    // getLastWorkout in api.js
     const workout = await API.getLastWorkout();
+    console.log("Last workout (index.js): ", workout);
+    
+    // Otherwise, if workout does exist, add to query string
     if (workout) {
       location.search = "?id=" + workout._id;
     } else {
+      // If no workouts are returned, d-none hides Continue button
       document.querySelector("#continue-btn").classList.add("d-none")
     }
   }
 }
-
